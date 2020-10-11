3443ec7602f87f9f0539772a4b4ea8e297bbb0a9
Saad Hmed <saad.ahmed2696@gmail.com>
2020-10-12 01:05:39 +0500

    useEffect, useContext

--- hookstart/src/App.js
+++ hookstart/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 // import ClassCounter from "./components/ClassCounter";
 import "./App.css";
-import DataFetching from "./components/DataFetching";
-import FiveClassCounter from "./components/FiveClassCounter";
-import IntervalClassCounter from "./components/IntervalClassCounter";
-import IntervalHookCounter from "./components/IntervalHookCounter";
-import MouseContainer from "./components/MouseContainer";
-import SixHookMouse from "./components/SixHookMouse";
+import ComponentC from "./components/ComponentC";
+// import DataFetching from "./components/DataFetching";
+// import FiveClassCounter from "./components/FiveClassCounter";
+// import IntervalClassCounter from "./components/IntervalClassCounter";
+// import IntervalHookCounter from "./components/IntervalHookCounter";
+// import MouseContainer from "./components/MouseContainer";
+// import SixHookMouse from "./components/SixHookMouse";
 // import HookCounter from "./components/1HookCounter";
 // import TwoHookCounter from "./components/TwoHookCounter";
 // import ThreeHookCounter from "./components/ThreeHookCounter";
 // import FourHookCounter from "./components/FourHookCounter";
 
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 function App() {
   return (
     <div className="App">
@@ -25,7 +28,12 @@ function App() {
       {/* <MouseContainer/> */}
       {/* <IntervalClassCounter/> */}
       {/* <IntervalHookCounter/> */}
-      <DataFetching/>
+      {/* <DataFetching /> */}
+      <UserContext.Provider value={"Saad"}>
+        <ChannelContext.Provider value={"yourdailyetech"}>
+          <ComponentC /> 
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
--- hookstart/src/components/DataFetching.js
+++ hookstart/src/components/DataFetching.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function DataFetching() {
-  const [posts, setPost] = useState([]);
-
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+  const [btnclickid, setButtonclickid] = useState(1);
+  const handleClick = () => {
+      setButtonclickid(id)
+  };
   useEffect(
     () => {
       async function fetchData() {
         try {
           const resp = await axios.get(
-            "https://jsonplaceholder.typicode.com/posts"
+            `https://jsonplaceholder.typicode.com/posts/${btnclickid}`
           );
           console.log(resp.data);
 
@@ -19,27 +23,38 @@ function DataFetching() {
         }
       }
       fetchData();
-    },[]
+    },
+    [btnclickid]
 
     //   .then((res) => {
     //     console.log(res);
     //     setPost(res.data);
     //   })
     //   .catch((err) => console.log(err));
-
   );
   return (
     <div>
-      {posts.slice(0,1).map((post) => (
+      <input
+        type="text"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      ></input>
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
+      {/* slice is used to iterate specific number of objects in array  */}
+      {/* posts.slice(25, 50).map((post) => ( */}
+      {/* {posts.map((post) => (
         <div key={post.id}>
           {" "}
           <h1>Title</h1>
           {post.title}
           <br></br>
-           <h1>Body</h1>
-          <div>{post.body}</div> 
+          <h1>Body</h1>
+          <div>{post.body}</div>
         </div>
-      ))}
+      ))} */}
+      <div>{post.body}</div>
     </div>
   );
 }
